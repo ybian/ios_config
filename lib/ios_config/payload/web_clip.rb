dabca478773d66dd7ba9ac066a3c35e08a54d09b
Ying Bian <bianying@gmail.com>
2014-09-26 06:50:14 +0800

    add FullScreen attribute to webclip

--- lib/ios_config/payload/web_clip.rb
+++ lib/ios_config/payload/web_clip.rb
@@ -2,7 +2,7 @@ module IOSConfig
   module Payload
     class WebClip < Base
 
-      attr_accessor :url, :label, :is_removable, :icon
+      attr_accessor :url, :label, :is_removable, :icon, :full_screen
 
       private
 
@@ -14,6 +14,7 @@ module IOSConfig
         p = {}
         p['URL']         = @url unless @url.nil?
         p['Label']       = @label unless @label.nil?
+        p['FullScreen']  = true unless @full_screen == false
         p['IsRemovable'] = @is_removable.nil? || @is_removable
         if @icon && File.exist?(@icon)
           p['Icon'] = File.open @icon
